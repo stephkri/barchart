1066ce44779ea67f7f8ef927f30fc4d7c1bc43b2
Stéphane Krims <stephane.krims@hotmail.ca>
2022-03-05 00:10:11 -0500

    Add hard data to input file and export

---
+++ input.js
@@ -0,0 +1,13 @@
+const hardData = [1, 2, 3, 5, 4];
+
+const hardOptions = {
+  title: "my chart",
+  titlePosition: "center",
+  barPosition: "center",
+  barColor: "green",
+  labelColor: "red",
+  barSpacing: "normal",
+  axes: { x: "abc", y: "123" }
+};
+
+module.exports = { hardData, hardOptions };
